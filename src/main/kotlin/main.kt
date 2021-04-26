c58ca6510eb293e100fb72db197d4e3ba7dba844
RicCampos <campos.ricardo12@gmail.com>
2021-04-27 00:18:10 +0100

    Serialização. Diferença entre Array/Map/Collection??? Falta formatar com aspas. Wrap output com {} ?

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,14 +1,69 @@
-enum class TypeEnum {
-    STRING,ARRAY,INT,OBJECT,ENUM
-}
-
 class JSONFile (){
     var objList: MutableList<JSONObject> = mutableListOf()
 
+    //Adiciona um novo objecto ao JSONFile
     fun addObject(e: JSONObject){
         objList.add(e)
     }
 
+    fun serialize(): String {
+
+        //Cria as funções do visitor
+            val v = object: Visitor {
+                var tree = ""
+                override fun visit(obj: JSONObject): String {
+                    tree += obj.name + "\n{\n"
+
+                    /*
+                    obj.elements.forEach {
+                        tree += visit(it)
+                    }
+                     */
+
+                    return ""
+                }
+                override fun visit(elem: JSONElement): String {
+                    tree += elem.name + ": "
+
+                    if(elem is JSONString){
+                        elem as JSONString
+                        tree += "'" + elem.value + "'"
+                    }
+                    else if(elem is JSONNumber) {
+                        elem as JSONNumber
+                        tree += elem.value
+                    }
+                    else if(elem is JSONArray || elem is JSONEnum) {
+                        elem as JSONArray
+                        tree += "["
+                        elem.value.forEach {
+                            if(it is JSONObject){
+                                tree += visit(it)
+                            }
+                            tree += "\"$it\","
+                        }
+                        tree = tree.substring(0,tree.length - 1) + "]"
+                    }
+                    else if(elem is JSONObject) {
+                        tree += visit(elem)
+                    }
+                    else {
+                        tree += "SHIIIT"
+                    }
+                    tree += ",\n"
+                    return tree
+                }
+            }
+
+        objList.forEach {
+            it.accept(v)
+            v.tree = v.tree.substring(0,v.tree.length - 2) + "\n},\n"
+        }
+
+        return v.tree.substring(0,v.tree.length - 2)
+    }
+
+    //Função de procura básica, devolve true se existe um objecto ou propriedade com o nome inserido ou false caso contrario
     fun search (name: String): Boolean{
         objList.forEach {
             if (it.name == name) {
@@ -23,17 +78,11 @@ class JSONFile (){
     }
 }
 
-abstract class JSONElement (val name: String, val type: TypeEnum) {
+abstract class JSONElement (val name: String) {
     abstract fun accept(v: Visitor)
 }
 
-class JSONObject
-    (
-    name: String,
-    type: TypeEnum,
-    val elements: MutableList<JSONElement>
-    )
-    : JSONElement(name,type) {
+class JSONObject(name: String, val elements: MutableList<JSONElement>) : JSONElement(name) {
 
     /*val nElements: MutableList<JSONElement> = mutableListOf()
 
@@ -41,6 +90,7 @@ class JSONObject
         nElements.add(e)
     }*/
 
+    //Função de procura básica, devolve true se existe um objecto ou propriedade com o nome inserido ou false caso contrario
     fun search (name: String): Boolean{
         elements.forEach {
             if (it.name == name) {
@@ -55,28 +105,83 @@ class JSONObject
     }
 
     override fun accept(v: Visitor) {
+        v.visit(this)
+        elements.forEach {
+            it.accept(v)
+        }
+        /*
         var s = ""
         s += v.visit(this) + "\n"
         elements.forEach {
             it.accept(v)
         }
+         */
     }
 
 }
 
-class JSONString (val name: String, val value: String)
-class JSONNumber (val name: String, val value: Int)
-class JSONArray (val name: String, val value: MutableList<Any>)
-class JSONEnum (val name: String, val value: MutableList<String>)
+class JSONString (name: String, val value: String): JSONElement(name) {
+    override fun accept(v: Visitor) {
+        v.visit(this)
+    }
+}
+class JSONNumber (name: String, val value: Int): JSONElement(name) {
+    override fun accept(v: Visitor) {
+        v.visit(this)
+    }
+}
+class JSONArray (name: String, val value: MutableList<Any>): JSONElement(name) {
+    override fun accept(v: Visitor) {
+        v.visit(this)
+    }
+}
+class JSONEnum (name: String, val value: MutableList<String>): JSONElement(name) {
+    override fun accept(v: Visitor) {
+        v.visit(this)
+    }
+}
 
 interface Visitor {
     fun visit(obj: JSONObject): String = ""
     fun visit(elem: JSONElement): String = ""
 }
 
+/*
+fun objToJson(o: Object): JSONElement {
+    //Não sei se os objectos vêm já formados ou em campos
+    //Método que passaria um objeto qualquer para um JSONElement
+}
+*/
+
 fun main () {
     var json = JSONFile()
 
+    val oString = JSONString("s","testVal")
+    val oNumber = JSONNumber("n",10)
+    val l = mutableListOf<JSONElement>()
+    l.add(oString)
+    l.add(oNumber)
+    val o = JSONObject("Test",l)
+
+    val yString = JSONString("Nome","Ronaldo")
+    val yNumber = JSONNumber("Idade",16)
+    val lt = mutableListOf<Any>()
+    lt.add("coisa1")
+    lt.add("coisa2")
+    lt.add("coisa3")
+    val yList = JSONArray("Lista", lt)
+    val l2 = mutableListOf<JSONElement>()
+    l2.add(yString)
+    l2.add(yNumber)
+    l2.add(yList)
+    val y = JSONObject("Aluno",l2)
+
+    json.addObject(o)
+    json.addObject(y)
+
+    println(json.serialize())
+
+    /*
     val v = object: Visitor {
         var tree = ""
         override fun visit(obj: JSONObject): String {
@@ -88,5 +193,9 @@ fun main () {
             return ""
         }
     }
+
+    o.accept((v))
+    println(v.tree)
+     */
 
 }
